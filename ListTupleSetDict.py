f6ece2375b13269e6d331e93ee3f819499651235
vinay kumar yadav <vinku920@gmail.com>
2022-02-05 15:33:33 +0530

    List Tuples Set Dict

---
+++ ListTupleSetDict.py
@@ -0,0 +1,19 @@
+a=[] #Empty List Contains any types of values
+b={} #Empty Dict contains key anf values pairs
+c=() #Empty tuple they are immutable
+d=set() # Empty Set contain different values only
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+a1=[
+    {
+        "RollNo": "35",
+        "Name": "ddbd"
+    },
+    {
+        "RollNo": "567",
+        "Name": "dbdb"
+    }
+]
+print((type(a1)))
