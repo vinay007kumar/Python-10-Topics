d3dec616b58b5ec586a18cb10945c85cf962fae1
vinay kumar yadav <vinku920@gmail.com>
2022-02-05 15:18:11 +0530

    Basix Syntax Added

---
+++ BasixSyntax.py
@@ -0,0 +1,18 @@
+# Basic Syntax (Data Types and Variable)
+# DATA TYPES
+str1= "My name is vinay" # Variables String
+int1 = 20 # variables int
+
+a1 = {1,2,3,"vinay"} # Set
+a2 = [1,2,3,"vin"] # List
+a3 = (1,2,3,4) # Tuple
+a4 = {
+    'a' : 36,
+    'b' : 'vinku'
+} # Dictonary
+
+
+print(type(a1))
+print(type(a2))
+print(type(a3))
+print(type(a4))
