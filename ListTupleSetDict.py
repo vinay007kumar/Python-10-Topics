726d883f2414ac8ed62f7534a8fbf5d9a69c6d2d
vinay kumar yadav <vinku920@gmail.com>
2022-02-07 13:41:18 +0530

    added some examples of list

--- ListTupleSetDict.py
+++ ListTupleSetDict.py
@@ -1,11 +1,11 @@
-a=[] #Empty List Contains any types of values
-b={} #Empty Dict contains key anf values pairs
-c=() #Empty tuple they are immutable
-d=set() # Empty Set contain different values only
-print(type(a))
-print(type(b))
-print(type(c))
-print(type(d))
+myList=[] #Empty List Contains any types of values
+myDict={} #Empty Dict contains key anf values pairs
+myTuple=() #Empty tuple they are immutable
+mySet=set() # Empty Set contain different values only
+print(type(myList))
+print(type(myDict))
+print(type(myTuple))
+print(type(mySet))
 a1=[
     {
         "RollNo": "35",
@@ -17,3 +17,16 @@ a1=[
     }
 ]
 print((type(a1)))
+myList2=[1,2,3,4,3,5,3,2,4,3]
+myList.append(34)
+# myList.extend(34) not possible as only extend another list
+myList.extend('40')
+myList.insert(2,40)
+myList.extend(myList2)
+print("3 Appres in myList is :",myList.count(3),"times.")
+myList.pop()
+print("Now 3 Appres in myList is :",myList.count(3),"times.")
+print(myList)
+
+mySet2 = set('45')
+print(mySet2)
